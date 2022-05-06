098f404a396a25116f195f70a57563cfc7cb8580
Boris Gubanov <44787162+boris-gu@users.noreply.github.com>
2022-05-06 20:10:40 +0300

    Fix speed & Ctrl+C

--- server-drone.py
+++ server-drone.py
@@ -8,7 +8,7 @@ from socket import *
 max_speed_xy = 1  # m/s
 max_speed_z = 0.5
 
-drone = Drone_api(redefine_zero_point=True)
+drone = Drone_api(redefine_zero_point=True, disable_signals=True)
 drone.start()
 print('Armed')
 drone.set_local_pose(0, 0, 3)
@@ -26,12 +26,10 @@ print(f'Start server, port: {port}')
 
 try:
     while not drone.is_shutdown():
-        print("test1")
-        data, address = udp_socket.recvfrom(8)  # fixme: Ctrl+C из-за ROS
-        print("test2")
+        print("OK")
+        data, address = udp_socket.recvfrom(8)
         print(data, addr)
         data = data.decode()
-        print(type(data), addr)  # todo: убрать
         c_t = data[0]
         if c_t in command_type:
             try:
@@ -43,18 +41,19 @@ try:
                 print('Incorrect command')
             else:
                 force /= 100
+                print(force)
                 if c_t == 'u':
-                    drone.set_velocity(0, 0, z=max_speed_z*force)
+                    drone.set_velocity(0, 0, max_speed_z*force)
                 elif c_t == 'd':
-                    drone.set_velocity(0, 0, z=-max_speed_z*force)
+                    drone.set_velocity(0, 0, -max_speed_z*force)
                 elif c_t == 'f':
                     drone.set_velocity(max_speed_xy*force, 0, 0)
                 elif c_t == 'b':
-                    drone.set_velocity(max_speed_xy*force, 0, 0)
+                    drone.set_velocity(-max_speed_xy*force, 0, 0)
                 elif c_t == 'l':
                     drone.set_velocity(0, max_speed_xy*force, 0)
                 elif c_t == 'r':
-                    drone.set_velocity(0, max_speed_xy*force, 0)
+                    drone.set_velocity(0, -max_speed_xy*force, 0)
         else:
             print('Incorrect command')
 

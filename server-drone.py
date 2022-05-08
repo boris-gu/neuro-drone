f7a57d00df62bc56041bdca7f30055448570e2d8
Boris Gubanov <44787162+boris-gu@users.noreply.github.com>
2022-05-08 19:43:38 +0300

    Add log in server-drone

--- server-drone.py
+++ server-drone.py
@@ -1,15 +1,26 @@
 #!/usr/bin/env python3
 
-# Модуль socket для сетевого программирования
 from drone_api import Drone_api
 from socket import *
+import argparse
+
+parser = argparse.ArgumentParser(description='Server-drone')
+parser.add_argument('-l', '--logfile', dest='logfile', action='store_false',
+                    help='if set, output is saved to a file "neuro_drone_log.txt"')
+args = parser.parse_args()
 
 # ДРОН
 max_speed_xy = 1  # m/s
 max_speed_z = 0.5
 
+if args.logfile:
+    log = open('neuro_drone_log.txt', 'w')
+
 drone = Drone_api(redefine_zero_point=True, disable_signals=True)
 drone.start()
+
+if args.logfile:
+    log.write('Armed\n')
 print('Armed')
 drone.set_local_pose(0, 0, 3)
 while not drone.point_is_reached():
@@ -22,11 +33,15 @@ addr = ('', port)
 
 udp_socket = socket(AF_INET, SOCK_DGRAM)
 udp_socket.bind(addr)
+if args.logfile:
+    log.write(f'Start server, port: {port}\n')
 print(f'Start server, port: {port}')
 
 try:
     while not drone.is_shutdown():
         data, client_arrd = udp_socket.recvfrom(8)
+        if args.logfile:
+            log.write(f'{data} {client_arrd}\n')
         print(data, client_arrd)
         data = data.decode()
         c_t = data[0]
@@ -59,7 +74,12 @@ try:
 except KeyboardInterrupt:
     pass
 except Exception as e:
+    if args.logfile:
+        log.write(e)
     print(e)
 finally:
+    if args.logfile:
+        log.write('Close server, landing')
+        log.close()
     print('\nClose server, landing')
     udp_socket.close()
